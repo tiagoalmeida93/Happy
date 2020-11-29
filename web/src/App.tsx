5124cc32347d41edcc82c8cff55e2abf2888bce6
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-11-29 17:01:02 -0300

    fix on tsconfig restart on yarn start

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,6 +9,12 @@ function App() {
     <div id="page-landing">
       <div className="content">
         <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        
       </div>
     </div>
   );
