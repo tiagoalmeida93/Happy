2594b58c0301888f5cc395d538c581658ee0f47e
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-12-16 21:29:28 -0300

    config ORM with sqlite

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from 'express';
+import './database/connection';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/users', (req, res) => {
-  return res.json({ message: "Hello" })
+  return res.json({ message: "Hello" });
 })
 
 app.listen(3333);
