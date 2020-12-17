82e775cee8a5112cfaffb9807158dee0a9dd90fb
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-12-17 20:35:49 -0300

    save orphanages on sqlite

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,39 @@
 import express from "express";
+import { getRepository } from "typeorm";
+import Orphanages from "./models/Orphanage";
+
 import "./database/connection";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  return res.json({ message: "Hello" });
+app.post("/orphanages", async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = req.body;
+
+  const orphanagesRepository = getRepository(Orphanages);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return res.json({ message: "Orphanages Create!" });
 });
 
 app.listen(3333);
