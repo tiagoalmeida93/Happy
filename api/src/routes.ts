4d7c593c8a1aac0018a257d20c49fa447d452ff0
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-12-17 21:59:13 -0300

    create the controller of orphanages

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,35 +1,8 @@
 import { Router } from "express";
-import { getRepository } from "typeorm";
-import Orphanages from "./models/Orphanage";
+import OrphanagesController from "./controllers/OrphanagesController";
 
 const routes = Router();
 
-routes.post("/orphanages", async (req, res) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  } = req.body;
-
-  const orphanagesRepository = getRepository(Orphanages);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends,
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return res.status(201).json(orphanage);
-});
+routes.post("/orphanages", OrphanagesController.create);
 
 export default routes;
